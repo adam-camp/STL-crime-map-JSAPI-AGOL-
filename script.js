0e719ff0dd641a37ae5b3d8e8e29cd65a0d56e05
adam-camp <98061122+adam-camp@users.noreply.github.com>
2022-02-07 13:33:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,62 @@
-require(["esri/Map", "esri/views/SceneView"], function (Map, SceneView) {
-        var map = new Map({
-          basemap: "topo-vector",
-          ground: "world-elevation"
+require([
+      "esri/Map",
+      "esri/layers/CSVLayer",
+      "esri/views/MapView",
+      "esri/config",
+      "esri/core/urlUtils",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      CSVLayer,
+      MapView,
+      esriConfig,
+      urlUtils
+    ) {
+
+      // If CSV files are not on the same domain as your website, a CORS enabled server
+      // or a proxy is required.
+     var url = "https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%202%20-%20ArcGIS%20JavaScript%20API/stl_crime_wgs_84.csv";
+     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
+
+      // Paste the url into a browser's address bar to download and view the attributes
+      // in the CSV file. These attributes include:
+      // * mag - magnitude
+      // * type - earthquake or other event such as nuclear test
+      // * place - location of the event
+      // * time - the time of the event
+
+        const template = {
+          title: "St Louis Crime",
+          content: "Reported case # {Crime} in District {district}, neighborhood {Neighborhood} at {ILEADSStreet}, coords: {Latitude}, {Longitude}."
+        };
+
+        const csvLayer = new CSVLayer({
+          url: url,
+          copyright: "St Louis Crime",
+          popupTemplate: template
         });
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          scale: 50000000,
-          center: [15, 65] // longitude, latitude
-        });
+        var symbol = {
+          type: "simple-marker", 
+          style: "triangle",
+          color:"yellow"
+        };
+
+      csvLayer.renderer = {
+        type: "simple", // autocasts as new SimpleRenderer()
+        symbol: symbol
+      };
+
+      var map = new Map({
+        basemap: "gray",
+        layers: [csvLayer]
       });
+
+      var view = new MapView({
+        container: "viewDiv",
+        center: [-90.18, 38.672],
+        zoom: 11,
+        map: map
+      });
+
+    });
